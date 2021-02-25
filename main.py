a4ed72a67ee1591f7370d5d7b045e10ef423c222
Jyao2001 <johnyao2001@gmail.com>
2021-02-24 22:44:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Find if number is divisible by number
 # Rules from https://www.mathsisfun.com/divisibility-rules.html
-def inputValue():
+def input_value():
     while True:
         value = input("Please enter a number or quit (q): \n")
         if value.isdigit():
@@ -13,14 +13,14 @@ def inputValue():
 
 
 # see if number is divisible by 1
-def divisibleBy1(number):
+def divisible_by_1(number):
     print(number + " is divisible by 1")
     return True
 
 
 # see if number is divisible by 2
 # Rule: The last digit is even
-def divisibleBy2(number):
+def divisible_by_2(number):
     if int(number) % 2 == 0:
         print(number + " is divisible by 2")
         return True
@@ -31,7 +31,7 @@ def divisibleBy2(number):
 
 # see if number is divisible by 3
 # Rule: The sum of the digits is divisible by 3
-def divisibleBy3(number):
+def divisible_by_3(number):
     sum = 0
     for i in range(len(number)):
         sum += int(number[i])
@@ -45,7 +45,7 @@ def divisibleBy3(number):
 
 # see if number is divisible by 4
 # Rule: The last 2 digits are divisible by 4
-def divisibleBy4(number):
+def divisible_by_4(number):
     lastTwoDigit = int(number) % 100
     if lastTwoDigit % 4 == 0:
         print(number + " is divisible by 4")
@@ -57,7 +57,7 @@ def divisibleBy4(number):
 
 # see if number is divisible by 5
 # Rule: The last digit is 0 or 5
-def divisibleBy5(number):
+def divisible_by_5(number):
     if int(number) % 10 == 0 or int(number) % 10 == 5:
         print(number + " is divisible by 5")
         return True
@@ -68,7 +68,7 @@ def divisibleBy5(number):
 
 # see if number is divisible by 6
 # Rule: Is even and is divisible by 3
-def divisibleBy6(number):
+def divisible_by_6(number):
     if int(number) % 2 == 0 and int(number) % 3 == 0:
         print(number + " is divisible by 6")
         return True
@@ -80,7 +80,7 @@ def divisibleBy6(number):
 # see if number is divisible by 7
 # Rule: Double the last digit and subtract it from a number made by the other digits.
 # The result must be divisible by 7.
-def divisibleBy7(number):
+def divisible_by_7(number):
     newnum = 0
     newnum = (2 * int(number) % 100) - int(number) // 10
     if newnum % 7 == 0:
@@ -93,7 +93,7 @@ def divisibleBy7(number):
 
 # see if number is divisible by 8
 # The last three digits are divisible by 8
-def divisibleBy8(number):
+def divisible_by_8(number):
     if (int(number) % 1000) % 8 == 0:
         print(number + " is divisible by 8")
         return True
@@ -104,7 +104,7 @@ def divisibleBy8(number):
 
 # see if number is divisible by 9
 # Rule: The sum of the digits is divisible by 9
-def divisibleBy9(number):
+def divisible_by_9(number):
     sum = 0
     for i in range(len(number)):
         sum += int(number[i])
@@ -122,15 +122,15 @@ def main():
     while True:
         number = inputValue()
         if str(number).isnumeric():
-            divisibleBy1(number)
-            divisibleBy2(number)
-            divisibleBy3(number)
-            divisibleBy4(number)
-            divisibleBy5(number)
-            divisibleBy6(number)
-            divisibleBy7(number)
-            divisibleBy8(number)
-            divisibleBy9(number)
+            divisible_by_1(number)
+            divisible_by_2(number)
+            divisible_by_3(number)
+            divisible_by_4(number)
+            divisible_by_5(number)
+            divisible_by_6(number)
+            divisible_by_7(number)
+            divisible_by_8(number)
+            divisible_by_9(number)
         else:
             break
 
